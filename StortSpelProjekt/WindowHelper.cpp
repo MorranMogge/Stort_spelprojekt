f85f43bbceba5e232ee028f8fed3eee0b37d3571
MorranMogge <77781581+MorranMogge@users.noreply.github.com>
2022-08-30 14:13:45 +0200

    Basic Window + ImGui
    Added the basics for creating a window and also added imgui

---
+++ StortSpelProjekt/WindowHelper.cpp
@@ -0,0 +1,51 @@
+#include "WindowHelper.h"
+#include <iostream>
+#include "imGUI\imconfig.h"
+#include "imGUI\imgui.h"
+#include "imGUI\imgui_impl_dx11.h"
+#include "imGUI\imgui_internal.h"
+#include "imGUI\imstb_rectpack.h"
+#include "imGUI\imstb_textedit.h"
+#include "imGUI\imstb_truetype.h"
+#include "imGUI\imgui_impl_win32.h"
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, message, wParam, lParam)) return true;
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+bool SetupWindow(HINSTANCE instance, UINT width, UINT height, int nCmdShow, HWND& window)
+{
+	const wchar_t CLASS_NAME[] = L"Window Class";
+
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = instance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	window = CreateWindowEx(0, CLASS_NAME, L"Projekt", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, width, height, nullptr, nullptr, instance, nullptr);
+
+	if (window == nullptr)
+	{
+		std::cerr << "HWND was, last error: " << GetLastError() << std::endl;
+		return false;
+	}
+
+	ShowWindow(window, nCmdShow);
+
+	return true;
+}
--- StortSpelProjekt/main.cpp
+++ StortSpelProjekt/main.cpp
@@ -1,13 +1,70 @@
+
 #include "Game.h"
 #include "Menu.h"
+#include "WindowHelper.h"
+#include "D3D11Helper.h"
+#include "MemoryLeackChecker.h"
 
-int main()
+#include "imGUI\imconfig.h"
+#include "imGUI\imgui.h"
+#include "imGUI\imgui_impl_dx11.h"
+#include "imGUI\imgui_internal.h"
+#include "imGUI\imstb_rectpack.h"
+#include "imGUI\imstb_textedit.h"
+#include "imGUI\imstb_truetype.h"
+#include "imGUI\imgui_impl_win32.h"
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstace, _In_ LPWSTR lpCmdLine, _In_ int nCmdShhow)
 {
-	State* currentState = new Menu();
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+
+	UINT WIDTH = 1080;
+	UINT HEIGHT = 720;
+	HWND window;
+
+	if (!SetupWindow(hInstance, WIDTH, HEIGHT, nCmdShhow, window))
+	{
+		ErrorLog::Log("Could not set up window!");
+		return -1;
+	}
+
+	ID3D11Device* device;
+	ID3D11DeviceContext* immediateContext;
+	IDXGISwapChain* swapChain;
+	ID3D11RenderTargetView* rtv;
+	ID3D11DepthStencilView* dsView;
+	ID3D11Texture2D* dsTexture;
+
+	D3D11_VIEWPORT viewport;
+
+	if (!SetupD3D11(WIDTH, HEIGHT, window, device, immediateContext, swapChain, rtv, dsTexture, dsView, viewport))
+	{
+		ErrorLog::Log("Could not set up D3D11!");
+		return -1;
+	}
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX11_Init(device, immediateContext);
+
+	State* currentState = DBG_NEW Menu();
 	GAMESTATE stateInfo = NOCHANGE;
 
-	while (stateInfo != EXIT)
+	MSG msg = {};
+
+	while (msg.message != WM_QUIT && stateInfo != EXIT)
 	{
+
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
 		stateInfo = currentState->Update();
 
 		if (stateInfo != NOCHANGE)
@@ -29,5 +86,21 @@ int main()
 	
 		currentState->Render();
 	}
+
+	#pragma region Deallocation
+	delete currentState;
+
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	device->Release();
+	immediateContext->Release();
+	swapChain->Release();
+	rtv->Release();
+	dsView->Release();
+	dsTexture->Release();
+	#pragma endregion
+	
 	return 0;
 }

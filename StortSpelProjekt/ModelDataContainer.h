48337b4655151b88d2f63d49ba81e17339c8cf5d
DAWA131 <eriksland@gmail.com>
2022-09-08 13:08:17 +0200

    ModelDataContainer
    Added MDC using maps. only simple mesh data works for now!

---
+++ StortSpelProjekt/ModelDataContainer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<iostream>
+#include<d3d11.h>
+#include<DirectXMath.h>
+#include<map>
+#include<iterator>
+#include<string>
+
+class MDC
+{
+public:
+	MDC(ID3D11Device* device);
+	~MDC();
+
+	bool hasItem(const std::string key);
+	ID3D11ShaderResourceView* getSrv(const std::string key);
+	ID3D11Buffer* getVertexBuffer(const std::string key);
+	ID3D11Buffer* getIndexBuffer(const std::string key);
+
+	void addSrv(std::string key, ID3D11ShaderResourceView* srv);
+	void addVertexBuffer(std::string key, ID3D11Buffer* vertexBuf);
+	void addIndexBuffer(std::string key, ID3D11Buffer* indexBuf);
+
+private:
+	ID3D11Device* device;
+
+	std::map<std::string, ID3D11ShaderResourceView*> srvMap;
+	std::map<std::string, ID3D11ShaderResourceView*>::iterator srvIt;
+
+	std::map<std::string, ID3D11Buffer*> vertexMap;
+	std::map<std::string, ID3D11Buffer*> indexMap;
+	std::map<std::string, ID3D11Buffer*>::iterator buffIt;
+
+
+};

e087f706b8c84028c374a67be2affa4a71c886c4
Viktor-N <81562062+Viktor-N@users.noreply.github.com>
2022-09-08 07:38:39 +0100

    Light handler class added

--- StortSpelProjekt/LightHandler.cpp
+++ StortSpelProjekt/LightHandler.cpp
@@ -1,13 +1,122 @@
 #include "LightHandler.h"
 
+
+//------------------------------------------------------------------------------- SETUP FUNCTIONS -------------------------------------------------------------------------------
+
+bool CreateLtBuffer(ID3D11Device* device, Microsoft::WRL::ComPtr<ID3D11Buffer>& lightBuffer, std::vector<Light>& lights, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>& lightBuffView)
+{
+	std::vector<LightStruct> structVector;
+	for (int i = 0; i < lights.size(); i++)
+	{
+		//Get data
+		DirectX::XMFLOAT3 position = lights.at(i).getPosition();
+		DirectX::XMFLOAT3 color = lights.at(i).getColor();
+		DirectX::XMFLOAT3 direction = lights.at(i).getDirection();
+
+		//Change to XMFLOAT4
+		DirectX::XMFLOAT4X4 matrix;
+		DirectX::XMFLOAT4 pos = DirectX::XMFLOAT4(position.x, position.y, position.z, 0);
+		DirectX::XMFLOAT4 col = DirectX::XMFLOAT4(color.x, color.y, color.z, 0);
+		DirectX::XMFLOAT4 dir = DirectX::XMFLOAT4(direction.x, direction.y, direction.z, 0);
+		float ca = lights.at(i).getConeAngle();
+		float typ = lights.at(i).getType();
+		DirectX::XMMATRIX tempMatrix = DirectX::XMMatrixTranspose(lights.at(i).getViewMatrix());
+		XMStoreFloat4x4(&matrix, tempMatrix);
+
+		//Create & push back struct
+		LightStruct lightArray(pos, col, dir, ca, typ, matrix);
+		structVector.push_back(lightArray);
+	}
+
+	D3D11_BUFFER_DESC cBuffDesc = { 0 };
+	cBuffDesc.ByteWidth = sizeof(LightStruct) * structVector.size();			//size of buffer //*nr of elements
+	cBuffDesc.Usage = D3D11_USAGE_DYNAMIC;										//sets interaction with gpu and cpu
+	cBuffDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;							//Specifies the type of buffer
+	cBuffDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;							//Specifies cpu acess
+	cBuffDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;				//Misc flags
+	cBuffDesc.StructureByteStride = sizeof(LightStruct);						//Size of each element in structure
+	D3D11_SUBRESOURCE_DATA cBufData = { 0 };									//holds matrix data
+	cBufData.pSysMem = structVector.data();										//pointer to data
+
+
+	//Create light buffer
+	HRESULT hr = device->CreateBuffer(&cBuffDesc, &cBufData, lightBuffer.GetAddressOf());
+
+
+	//ShaderResource view 
+	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc = {};
+	shaderResourceViewDesc.Format = DXGI_FORMAT_UNKNOWN;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+	shaderResourceViewDesc.Buffer.FirstElement = 0;
+	shaderResourceViewDesc.Buffer.NumElements = structVector.size();
+
+	//create shader resource view 
+	device->CreateShaderResourceView(lightBuffer.Get(), &shaderResourceViewDesc, lightBuffView.GetAddressOf());
+	return !FAILED(hr);
+}
+
+//------------------------------------------------------------------------------- CONSTRUCTOR -------------------------------------------------------------------------------
+
+
 LightHandler::LightHandler(UINT winWidth, UINT winHeight)
 	:shadowHeight(winHeight), shadowWidth(winWidth)
 {
 }
+
+// ------------------------------------------------------------------------------- FUNCTIONS -------------------------------------------------------------------------------
 
 void LightHandler::addLight(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 color, DirectX::XMFLOAT3 direction, DirectX::XMFLOAT3 UpDir, int type, float coneAngle)
 {
 	//Skapa ljus
 	Light templight( color, position, direction, UpDir, coneAngle, type);
 	this->lights.push_back(templight);
+}
+
+void LightHandler::finalizeLights(ID3D11Device* device, ID3D11DeviceContext* immediateContext)
+{
+
+	//lägg till info från light i buffers 
+	int nrOfLights = this->lights.size();
+
+	//Create depth stencil, textureArr, depthViewArr & shader resource view
+	//if (!CreateDepthStencil(device, this->shadowWidth, this->shadowHeight, this->depthTextures, this->depthViews, this->shaderView, nrOfLights))
+	//{
+	//	std::cerr << "error creating Dstencil/Srv!" << std::endl;
+	//}
+
+	//Create buffer containing projection & view Matrix data
+	//for (int i = 0; i < nrOfLights; i++)
+	//{
+	//	ComPtr <ID3D11Buffer> tempBuffer;
+	//	if (!CreateViewBuffer(device, tempBuffer, this->lights.at(i)))
+	//	{
+	//		std::cerr << "error creating viewBuffer!" << std::endl;
+	//	}
+	//	this->viewBuffers.push_back(tempBuffer);
+	//}
+
+	//Create single buffer containing all light data
+	//if (!CreateLtBuffer(device, this->lightBuffer, this->Pointlights, this->lightBuffView))
+	//{
+	//	std::cerr << "error creating lightBuffer!" << std::endl;
+	//}
+
+	//Create buffer with int for nrof lights.
+	//if (!CreateNumLTBuffer(device, this->numLightBuffer, nrOfLights))
+	//{
+	//	std::cerr << "error creating numlightBuffer!" << std::endl;
+	//}
+
+	//Create Meshes
+
+}
+
+ID3D11Buffer* LightHandler::getViewBuffer(int ltIndex) const
+{
+	return this->viewBuffers.at(ltIndex).Get();
+}
+
+int LightHandler::getNrOfLights() const
+{
+	return this->lights.size();
 }

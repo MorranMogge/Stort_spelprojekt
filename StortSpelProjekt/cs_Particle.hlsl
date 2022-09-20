b1a3da8202ab1187d21bd9eab103b657a157d9a0
nightcorelv <38535966+nightcorelv@users.noreply.github.com>
2022-09-20 11:33:32 +0200

    particle backup

--- StortSpelProjekt/cs_Particle.hlsl
+++ StortSpelProjekt/cs_Particle.hlsl
@@ -1,14 +1,10 @@
-RWBuffer<float> particlePositions : register(u0);
 
-cbuffer timeValue : register(b0)
-{
-    float time;
-};
+#include "particle_Include.hlsli"
 
 [numthreads(32, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
-    particlePositions[DTid.x * 3] = cos(time * 0.1f * DTid.x);
+    particlePositions[DTid.x * 3] = cos(deltaTime * 0.1f * DTid.x);
     
     if (particlePositions[DTid.x * 3 + 1] > 15.0f) // if y position is greater than 15
     {
@@ -16,8 +12,8 @@ void main( uint3 DTid : SV_DispatchThreadID )
     }
     else
     {
-        particlePositions[DTid.x * 3 + 1] += (1 - (sin(time * 3.1415926 * DTid.x) * 0.5f + 0.5f)) * 0.003f;
+        particlePositions[DTid.x * 3 + 1] += (1 - (sin(deltaTime * 3.1415926 * DTid.x) * 0.5f + 0.5f)) * 0.003f;
     }
     
-    particlePositions[DTid.x * 3 + 2] = sin(time * 0.1f * DTid.x);
+    particlePositions[DTid.x * 3 + 2] = sin(deltaTime * 0.1f * DTid.x);
 }

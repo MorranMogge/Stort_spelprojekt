edb7d2edac959e5994303a90a9af4773edf61e8e
xbeamlightx <forslund.elliot@mail.com>
2022-10-03 09:02:49 +0200

    changed to const reference

--- StortSpelProjekt/GameObject.h
+++ StortSpelProjekt/GameObject.h
@@ -16,22 +16,22 @@ protected:
 	DirectX::XMFLOAT3 scale;
 
 public:
-	GameObject(Mesh* useMesh, DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 rot, int id, DirectX::XMFLOAT3 scale = DirectX::XMFLOAT3(1, 1, 1));
-	GameObject(std::string meshPath, DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 rot, int id, DirectX::XMFLOAT3 scale = DirectX::XMFLOAT3(1, 1, 1));
+	GameObject(Mesh* useMesh, const DirectX::XMFLOAT3& pos, const DirectX::XMFLOAT3& rot, const int& id, const DirectX::XMFLOAT3& scale = DirectX::XMFLOAT3(1, 1, 1));
+	GameObject(const std::string& meshPath, const DirectX::XMFLOAT3& pos, const DirectX::XMFLOAT3& rot, const int& id, const DirectX::XMFLOAT3& scale = DirectX::XMFLOAT3(1, 1, 1));
 	GameObject();
 	virtual ~GameObject();
-	void movePos(DirectX::XMFLOAT3 offset);
-	void setPos(DirectX::XMFLOAT3 pos);
-	void setRot(DirectX::XMFLOAT3 rot);
-	void setScale(DirectX::XMFLOAT3 scale);
+	void movePos(const DirectX::XMFLOAT3& offset);
+	void setPos(const DirectX::XMFLOAT3& pos);
+	void setRot(const DirectX::XMFLOAT3& rot);
+	void setScale(const DirectX::XMFLOAT3& scale);
 	DirectX::XMFLOAT3 getPos() const;
 	DirectX::SimpleMath::Vector3 getPosV3()const;
 	DirectX::XMFLOAT3 getRot() const;
 	DirectX::XMFLOAT3 getScale() const;
 	Bound* getBounds() const;
 	void updateBuffer();
-	void setMesh(std::string meshPath);
+	void setMesh(const std::string& meshPath);
 	void setMesh(Mesh*);
-	bool withinRadious(GameObject *object ,float radius) const;
+	bool withinRadious(GameObject *object , const float& radius) const;
 	virtual void draw();
 };

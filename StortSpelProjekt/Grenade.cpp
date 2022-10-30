fcb59efc8ac1b7f7ee308d593b4436acb28f2086
nightcorelv <liya20@student.bth.se>
2022-10-30 13:42:09 +0100

    grenade icon

--- StortSpelProjekt/Grenade.cpp
+++ StortSpelProjekt/Grenade.cpp
@@ -49,7 +49,7 @@ Grenade::Grenade(const std::string& objectPath, const DirectX::XMFLOAT3& pos, co
 	float constant = 4.0f;
 	DirectX::XMFLOAT3 upDir = this->getUpDirection();
 	DirectX::XMFLOAT3 iconPos(upDir.x * constant, upDir.y * constant, upDir.z * constant);
-	std::vector<std::string> tempStr{ "icon_sword.png", "team_b_0.png" };
+	std::vector<std::string> tempStr{ "icon_boom.png", "icon_boom2.png" };
 	this->itemIcon = new BilboardObject(tempStr, iconPos);
 	this->itemIcon->setOffset(constant);
 }

532439a9c498df9796f7e4db053694afbb5593cc
Viktor-N <81562062+Viktor-N@users.noreply.github.com>
2022-11-30 15:10:11 +0000

    normal fix

--- StortSpelProjekt/DomainShader.hlsl
+++ StortSpelProjekt/DomainShader.hlsl
@@ -56,7 +56,7 @@ PSInput main(
     output.worldPosition = mul(output.position, worldMatrix);
     output.position = mul(output.worldPosition, camViewProj);
 
-    output.normal = uvw.x * tri[0].normal + uvw.y * tri[1].normal + uvw.z * tri[2].normal;
+    output.normal = normalize(mul(uvw.x * tri[0].normal + uvw.y * tri[1].normal + uvw.z * tri[2].normal, (float3x3)worldMatrix));
     
     output.uv = uvw.x * tri[0].uv + uvw.y * tri[1].uv + uvw.z * tri[2].uv;
 
